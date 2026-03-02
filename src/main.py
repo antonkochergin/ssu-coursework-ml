42e4c2c4d758b38860cf5c24f7ec9bfdc102b5b9
antonkochergin <antonkochergin4@gmail.com>
2026-03-02 08:45:51 +0400

    Сделан второй шаг с контрастностью. Написана функция draw_grid для сетки

--- src/main.py
+++ src/main.py
@@ -4,7 +4,31 @@ from rembg import remove
 from PIL import Image
 
 img = cv2.imread("./foots/1/IMG_0315.jpg")
-img = cv2.resize(img, (img.shape[1] // 4, img.shape[0] // 4))
+img = cv2.resize(img, (img.shape[1] // 5, img.shape[0] // 5))
+
+
+# cv2.imshow("Ishodnik", img)
+
+
+def draw_grid(image, cell_size=50, color=(0, 255, 0), thickness=1):
+    """
+    Рисует сетку на изображении
+    """
+    img_copy = image.copy()
+    h, w = img_copy.shape[:2]
+
+    # Рисуем вертикальные линии
+    for x in range(0, w, cell_size):
+        cv2.line(img_copy, (x, 0), (x, h), color, thickness)
+
+    # Рисуем горизонтальные линии
+    for y in range(0, h, cell_size):
+        cv2.line(img_copy, (0, y), (w, y), color, thickness)
+
+    return img_copy
+
+
+img = draw_grid(img, cell_size=25, color=(40, 40, 40), thickness=1)
 
 
 # ВОПРОС: тут надо подавать изображение с параметрами или можно просто подать изображение и уже из него извлечь данные
@@ -27,5 +51,10 @@ def draw_contour(image, width, height, channels):
     return gray
 
 
-cv2.imshow("Result", draw_contour(img, img.shape[1], img.shape[0], img.shape[2]))
+gray = draw_contour(img, img.shape[1], img.shape[0], img.shape[2])
+# cv2.imshow("Result", gray)
+
+ret, result = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+cv2.imshow("Result", result)
 cv2.waitKey(0)
